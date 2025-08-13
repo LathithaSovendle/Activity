313d350dc4d88177ee4d61f26ae2da5b2b11af67
LathithaSovendle <st10476533@vcconnect.edu.za>
2025-08-13 15:09:57 +0200

    Added screens to my code

--- App.tsx
+++ App.tsx
@@ -1,34 +1,107 @@
 
 import { StyleSheet, Text, View, TextInput,Button, Image} from 'react-native';
+import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+return(
+ <NavigationContainer>
+<Stack.Navigator>
+<Stack.Screen name ="Home" component={MainScreen}/>
+<Stack.Screen name ="ViewDetails" component={ViewDetails}/>
+</Stack.Navigator>
+ </NavigationContainer>
+
+);
+};
+
+
+function MainScreen({navigation})
+ {
+
+  const[Name, setName] = useState('');
+  const[Surname, setSurname] = useState('');
+
+  console.log("App starting up now.")
   return (
-    <View style={styles.container}>
-<View>
-    <Image source ={require('./Activity/assets/welcome_to_react.png')}/>
+   
+    <View>
+<View style ={styles.maiPicture}>
+    <Image style ={styles.ImageSize}
+    source ={require('./img/welcome_to_react.png')}/>
 </View>
+    <Text style ={styles.welcomeText}>Welcome my react App!</Text>
 
-    <Text style ={styles.welcomeText}>Welcome your react App!</Text>
+    <View style={styles.InputFlex}>
+    <Text style ={styles.HeadingText}>Enter Name:</Text>
+    <TextInput style ={styles.InputBoxs}
+     placeholder ="First Name"
+     onChangeText = {newText => setName(newText)}
 
-    <Text style ={{fontSize:23}}>Enter Name:</Text>
-    <TextInput placeholder ="First Name"/>
-    <Text style ={{fontSize:23}}>Enter Surname:</Text>
-    <TextInput placeholder="Surname"></TextInput>
-<Button title ="Add user"></Button>
+     />
 
     </View>
-    
+    <Text style = {styles.HeadingText}>Enter Surname:</Text>
+    <TextInput style= {styles.InputBoxs} placeholder="Surname"></TextInput>
+
+
+     
+  
+<Button title ="Add user"
+ onPress ={()=>{
+  navigation.navigate('ViewDetails');
+  console.log("The user name is:" +Name +"Surname: " + Surname)
+ }}/>
+
+    </View>
+
   );
 }
+
+function ViewDetails() {
+  return(
+    <View style ={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Name: ### Surname:####</Text>
+    </View>
+
+  );
+};
 const styles = StyleSheet.create({
   welcomeText:{
-    backgroundColor:'black',
+    backgroundColor:'#E3ECED',
     paddingTop: 40,
-    color:'purple',
+    color:'blue',
     fontWeight:'bold',
-    fontSize: 38,
+    fontSize: 48,
     textAlign: 'center',
-  }
+  },
+  ImageSize: {
+    width: 350,
+    height: 350
+  },
+maiPicture:{
+paddingTop: 40,
+backgroundColor:'#E3ECED',
+justifyContent:'center',
+alignItems: 'center',
+
+},
+InputFlex:{
+  backgroundColor:'#D2E5E9',
+ 
+  flexDirection:'row',
+  marginTop:30,
+
+},
+InputBoxs: {
+  backgroundColor:'#D2E5E9',
+  fontSize: 23,
+},
+HeadingText: {
+fontSize:32
+}
 });
 
 
